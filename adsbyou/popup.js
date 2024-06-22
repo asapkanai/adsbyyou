c2a745c12b3f76a0ae790639295f901e9ca974c4
Kanai <142159088+asapkanai@users.noreply.github.com>
2024-06-22 12:41:58 +0300

    Add files via upload
    This my first prototype of adsbyyou.com without backend

---
+++ adsbyou/popup.js
@@ -0,0 +1,7 @@
+function openForm(){
+    document.getElementById("jav-form").style.display ="block";
+    document.getElementById("jav-form").style.margin = "auto";
+}
+function closeForm(){
+    document.getElementById("jav-form").style.display ="none";
+}
